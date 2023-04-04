45708ef5f940665169b0de9de277631f79a91d5c
Diego Saavedra <dsaavedra88@gmail.com>
2023-04-03 20:02:27 -0500

    Pandas, Numpy, Matplotlib, Skcikit-learn, Seaborn

---
+++ Code/Ejercicios/main.py
@@ -0,0 +1,7 @@
+from modulo import suma as s, multiplicacion as mult
+
+suma1 = s(3, 4)
+print(suma1)
+
+multiplicacion1 = mult(3, 4)
+print(multiplicacion1)
---
+++ Code/Ejercicios/modulo.py
@@ -0,0 +1,11 @@
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
